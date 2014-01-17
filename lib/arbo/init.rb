bb169c5eef4abf313dee4d59fdb90d316eda884a
BennyHallett <benny.hallett@gmail.com>
2014-01-18 10:38:53 +1100

    Added test for init command

--- lib/arbo/init.rb
+++ lib/arbo/init.rb
@@ -1,7 +1,11 @@
+require 'arbo/arbo_db'
+
 class InitCommand
 
   def process(global_options, options, args, crypto)
     raise 'Cannot process init command without crypto' unless crypto
+    db  = ArboDb.new global_options[:file]
+    db.init crypto
   end
   
 end
--- test/init_command_test.rb
+++ test/init_command_test.rb
@@ -1,19 +1,32 @@
 require 'test_helper'
+require 'json'
 require 'mocha/setup'
 require 'arbo/init'
+require 'fileutils'
 
 class InitCommandTest < Test::Unit::TestCase
 
   def setup
     @init = InitCommand.new
+    @crypto = mock('object')
+    @file = '/tmp/tmpdb'
+    @globals = { :file => @file, 'file' => @file, :f => @file, 'f' => @file }
   end
 
   def teardown
+    if File.exist? @file
+      FileUtils.rm @file
+    end
   end
 
   def test_attempt_to_init_without_crypto_fails
     assert_raise RuntimeError do
-      @init.process Array.new, Array.new, Array.new, nil
+      @init.process Hash.new, Hash.new, Array.new, nil
     end
+  end
+
+  def test_init_calls_encrypt
+    @crypto.expects(:encrypt).with(Hash.new.to_json)
+    @init.process @globals, Hash.new, Array.new, @crypto
   end
 end
