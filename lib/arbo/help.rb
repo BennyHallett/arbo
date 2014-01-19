63e90cf3bcf881b31df3bb10448869a512aa5d18
BennyHallett <benny.hallett@gmail.com>
2014-01-20 08:12:09 +1100

    Updated help text. Fixed related test.

--- lib/arbo/help.rb
+++ lib/arbo/help.rb
@@ -1,7 +1,27 @@
 class HelpCommand
 
   def process(global_options, options, args)
-    return 'Arbo - the command line password manager.'
+    return 'Arbo - the command line password manager.
+
+USAGE: arbo [-f|--file $filepath] $command $arguments...
+
+COMMANDS:
+
+  help:     Displays this help text.
+  init:     Initialises the database. Will fail if one already exists.
+  set:      Sets the password for a given key.
+  get:      Retrieves the password for a given key.
+  list:     List all of the keys in the database.
+  delete:   Delete a password from the database.
+  generate: Generate a random password for a given key.
+
+  For more information on these commands, please use `arbo help $command`
+
+GLOBAL FLAGS:
+
+  -f | --filename:    Specify the database file to use.
+                      Default is ~/.arbodb
+'
   end
   
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,6 +6,26 @@ class Test::Unit::TestCase
 
   # Add global extensions to the test case class here
 
-  @@help_text = 'Arbo - the command line password manager.'
+  @@help_text = 'Arbo - the command line password manager.
+
+USAGE: arbo [-f|--file $filepath] $command $arguments...
+
+COMMANDS:
+
+  help:     Displays this help text.
+  init:     Initialises the database. Will fail if one already exists.
+  set:      Sets the password for a given key.
+  get:      Retrieves the password for a given key.
+  list:     List all of the keys in the database.
+  delete:   Delete a password from the database.
+  generate: Generate a random password for a given key.
+
+  For more information on these commands, please use `arbo help $command`
+
+GLOBAL FLAGS:
+
+  -f | --filename:    Specify the database file to use.
+                      Default is ~/.arbodb
+'
   
 end
