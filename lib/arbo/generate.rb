3c52e9af56efb969aeb4f03295800a109438be7e
BennyHallett <benny.hallett@gmail.com>
2014-01-21 08:39:17 +1100

    Initial tests for generate command ensuring dependencies

---
+++ lib/arbo/generate.rb
@@ -0,0 +1,16 @@
+require 'arbo/arbo_db'
+
+class GenerateCommand
+
+  def process(global_options, options, args, crypto, generator)
+    raise 'Cannot process set command without crypto' unless crypto
+    raise 'Cannot process set command without generator' unless generator
+    #raise "Set command requires one key argument. You have provided #{args.length}." unless args.length == 1
+
+    #key = args.first
+  
+    #db  = ArboDb.new global_options[:file]
+    #db.set key, pw, crypto
+  end
+  
+end
---
+++ test/generate_command_test.rb
@@ -0,0 +1,36 @@
+require 'test_helper'
+require 'json'
+require 'mocha/setup'
+require 'arbo/generate'
+require 'fileutils'
+
+class GenerateCommandTest < Test::Unit::TestCase
+
+  def setup
+    @generate = GenerateCommand.new
+    @crypto = mock('object')
+    @generator = mock('object')
+    @file = '/tmp/tmpdb'
+    FileUtils.touch @file
+    @globals = { :file => @file, 'file' => @file, :f => @file, 'f' => @file }
+    @key = 'testkey'
+  end
+
+  def teardown
+    if File.exist? @file
+      FileUtils.rm @file
+    end
+  end
+
+  def test_attempt_to_generate_without_crypto_fails
+    assert_raise RuntimeError do
+      @generate.process Hash.new, Hash.new, Array.new, nil, @generator
+    end
+  end
+
+  def test_attempt_to_generate_without_generator_fails
+    assert_raise RuntimeError do
+      @generate.process Hash.new, Hash.new, Array.new, @crypto, nil
+    end
+  end
+end
